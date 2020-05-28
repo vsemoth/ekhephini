00028d7949faf82ab42db53bf0a5eb32b93bb3a1
Lwandile Rozani <vsemoth@gmail.com>
2020-05-28 14:19:40 +0200

    Shotcast Broadcast Source

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <meta charset="utf-8">
-            <title>Ilizwilenqubela | Ekhephini Community Radio | ECR FM</title>
-        <script src="hls.min.js"></script>
+        <meta charset="utf-8" />
+        <title>Ekhephini Community Radio</title>
         <style>
             body {
                 background-color: #000;
@@ -17,5 +16,6 @@
             <video style="background-image: url('92382088_935896283529581_6975350407115571200_o.jpg'); background-size: 600px; background-repeat: no-repeat; width: 600px;" id="video" controls autoplay src="http://185.247.119.138:8000/listen.html"></video>
 
         </div>
-        </div> 
+        </div>        
+    </body>
 </html>
